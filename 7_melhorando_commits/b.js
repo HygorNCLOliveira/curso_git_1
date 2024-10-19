abacb9f02001c239725071e5ef00f2fa1c8d1630
Hygor Oliveira <152923638+HygorNCLOliveira@users.noreply.github.com>
2024-10-19 12:20:21 -0300

    7_melhorando_commits

---
+++ 7_melhorando_commits/b.js
@@ -0,0 +1,18 @@
+console.log("Testando branch privada, arquivo criado dentro do branch privada")
+
+console.log("Teste")
+
+console.log("Y")
+console.log("Y")
+console.log("Y")
+console.log("Y")
+console.log("Y")
+console.log("Y")
+console.log("Y")
+
+// Melhorando os commits
+// Funcao que mostra o carrinho de compras (só exemplo)
+function addCart(p){
+    cart = {}
+    return cart
+}
